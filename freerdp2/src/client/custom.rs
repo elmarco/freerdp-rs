21d267b8021398f4510b332fe8d3a159ba07c736
Marc-André Lureau <marcandre.lureau@redhat.com>
2023-06-13 10:39:50 +0200

    Fix a clippy warning
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- freerdp2/src/client/custom.rs
+++ freerdp2/src/client/custom.rs
@@ -16,7 +16,7 @@ impl Custom {
     pub(super) fn new<H>(handler: H) -> *mut c_void {
         Box::into_raw(Box::new(Custom {
             handler: Box::into_raw(Box::new(handler)) as *mut _,
-            free: |raw| unsafe { drop(Box::from_raw(raw)) },
+            free: |raw| unsafe { drop(Box::from_raw(raw as *mut H)) },
         })) as *mut _
     }
 }
